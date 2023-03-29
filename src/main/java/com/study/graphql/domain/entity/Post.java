7c243f7b216012f472cc115b3c1d48bd32fe48b5
임서영 <sylim0318@naver.com>
2023-03-30 08:04:08 +0900

    post entity

---
+++ src/main/java/com/study/graphql/domain/entity/Post.java
@@ -0,0 +1,30 @@
+package com.study.graphql.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "post")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private LocalDate date;
+
+}
