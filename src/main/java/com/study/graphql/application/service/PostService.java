4ca6bd4d27143dde009f455d70b7f862be4ce81f
임서영 <sylim0318@naver.com>
2023-03-31 08:05:36 +0900

    post service

--- src/main/java/com/study/graphql/application/service/PostService.java
+++ src/main/java/com/study/graphql/application/service/PostService.java
@@ -1,7 +1,18 @@
 package com.study.graphql.application.service;
 
+import com.study.graphql.application.dto.PostRequest;
+import com.study.graphql.application.dto.PostResponse;
+
+import java.util.List;
+
 public interface PostService {
 
-    int createPost();
+    int createPost(PostRequest request);
+
+    PostResponse getPost(Integer postId);
+
+    List<PostResponse> getPosts();
+
+    int updatePost(Integer post, PostRequest request);
 
 }
--- src/main/java/com/study/graphql/application/service/PostServiceImpl.java
+++ src/main/java/com/study/graphql/application/service/PostServiceImpl.java
@@ -1,11 +1,37 @@
 package com.study.graphql.application.service;
 
+import com.study.graphql.application.dto.PostRequest;
+import com.study.graphql.application.dto.PostResponse;
+import com.study.graphql.domain.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
-public class PostServiceImpl {
+public class PostServiceImpl implements PostService {
 
+    private final PostRepository postRepository;
+
+    @Override
+    public int createPost(PostRequest request) {
+        return 0;
+    }
+
+    @Override
+    public PostResponse getPost(Integer postId) {
+        return null;
+    }
+
+    @Override
+    public List<PostResponse> getPosts() {
+        return null;
+    }
+
+    @Override
+    public int updatePost(Integer post, PostRequest request) {
+        return 0;
+    }
 
 }
