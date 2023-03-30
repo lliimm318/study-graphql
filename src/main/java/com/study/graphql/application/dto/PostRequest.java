5704d43036b80957a18786eeab39ffca71410f6d
임서영 <sylim0318@naver.com>
2023-03-30 10:18:44 +0900

    post request

---
+++ src/main/java/com/study/graphql/application/dto/PostRequest.java
@@ -0,0 +1,14 @@
+package com.study.graphql.application.dto;
+
+import lombok.Getter;
+
+@Getter
+public class PostRequest {
+
+    private String title;
+
+    private String content;
+
+    private String writer;
+
+}
